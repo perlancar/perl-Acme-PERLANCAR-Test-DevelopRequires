307f2e0e098238d4abd495163eae94ba850bde6e
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-10-18 18:02:51 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Test/DevelopRequires.pm
@@ -0,0 +1,18 @@
+package Acme::PERLANCAR::Test::DevelopRequires;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+# from Regexp::IPv4
+our $re = qr/(?^:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})(?:\.(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})){3}))/;
+
+1;
+# ABSTRACT: Distribution that has a DevelopRequires prereq
+
+=head1 DESCRIPTION
+
+This distribution is created for testing.
